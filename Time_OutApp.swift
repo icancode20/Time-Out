57d1f1ecebba7d4e9f5403baa57f2fa0be9e0921
icancode20 <67137733+icancode20@users.noreply.github.com>
2022-07-23 12:52:34 -0700

    Add files via upload

---
+++ Time_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_OutApp.swift
+//  Shared
+//
+//  Created by HomeMac on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Time_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
